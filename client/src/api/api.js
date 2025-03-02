9d427416c2dd010a28d1c7a54a906302f1b2bce6
Dor Wandi <dorwandrov@gmail.com>
2025-03-02 13:34:08 +0200

    update API baseURL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
+
+
+const baseURL=proccess.env.NODE_ENV==='production' ? 'kapilka-agvz.onrender.com/api' : 'http://localhost:3000/api';
 //הגדרת קובץ בסיס לכל קריאות ה api
 export default axios.create({
 
-    baseURL: 'http://localhost:3000/api',
+    baseURL,
     headers: {
         'Content-Type': 'application/json',
     },
