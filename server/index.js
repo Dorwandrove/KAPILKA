3b01dbb275a20a8132fbbf30a19295461ea52dba
Dor Wandi <dorwandrov@gmail.com>
2025-03-09 13:37:16 +0200

    fix deploy 6 - it's not over yet

--- server/index.js
+++ server/index.js
@@ -23,6 +23,11 @@ app.use(
 
 app.use('/api',router);
 
+app.get('*', (req, res
+  ) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(3000, () => {
   connectDB();
   console.log('Server is running on http://localhost:3000');
