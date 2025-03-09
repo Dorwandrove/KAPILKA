f8c4ed902a9d2f6000266e0b3e2032fcd7cc7e62
Dor Wandi <dorwandrov@gmail.com>
2025-03-09 13:33:08 +0200

    fix deploy 5 - end game

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const baseURL=proccess.env.NODE_ENV==='production' ? 'kapilka-agvz.onrender.com/api' : '/api';
+const baseURL=proccess.env.NODE_ENV==='production' ? '/api': 'http://localhost:3000/api';//כתובת השרת של האפליקציה
 //הגדרת קובץ בסיס לכל קריאות ה api
 export default axios.create({
 
