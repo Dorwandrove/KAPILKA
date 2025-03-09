e712945c3c79fc68379dde55ae463aaa47d7954f
Dor Wandi <dorwandrov@gmail.com>
2025-03-09 13:42:59 +0200

    fix api - correct typo in process.env and update script source in index.html

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const baseURL=proccess.env.NODE_ENV==='production' ? '/api': 'http://localhost:3000/api';//כתובת השרת של האפליקציה
+const baseURL=process.env.NODE_ENV==='production' ? '/api': 'http://localhost:3000/api';//כתובת השרת של האפליקציה
 //הגדרת קובץ בסיס לכל קריאות ה api
 export default axios.create({
     baseURL,
